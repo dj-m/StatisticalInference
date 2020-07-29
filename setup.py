f3e760551f28bfbb4fc8eff514a55a22399210fe
dj-m <domingomoronta@gmail.com>
2020-07-29 13:20:24 -0700

    Applied cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Case study to learn about frequentist inference. Part A are Pythonic implementations of underlying frequentist inference. Part B is the application of those implementatiosn on a reald--world scenario.',
+    author='Domingo Moronta',
+    license='',
+)
